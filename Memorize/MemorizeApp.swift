4690eac617504fa422128ef6681f19035a5406dd
Aadithya-J <jlaadithya@gmail.com>
2024-01-24 22:17:43 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Aadithya Jayakaran on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
